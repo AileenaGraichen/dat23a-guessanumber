5ea4c21181251093f9da7d49dd199f6526a19501
Aileena <aile0074@stud.kea.dk>
2023-09-01 09:52:06 +0200

    final version - improvements

--- src/main/java/Tasks.java
+++ src/main/java/Tasks.java
@@ -3,34 +3,45 @@ import java.util.Scanner;
 
 public class Tasks {
     public static void main(String[] args) {
-        int secretNumber = -1;
-        int guessedNumber = -1;
+        final int MAX_GUESSES = 3;
+        int secretNumber;
+        int guessedNumber;
         int numberOfGuesses = 0;
         boolean keepRunning = true;
         Random random = new Random();
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.print("====================================");
-        System.out.println("\nWelcome to 'Guess A Number!'");
-        System.out.println("\nI am thinking of a number between 1 and 10.\n");
+        System.out.print("====================================================");
+        System.out.println("\n=   Welcome to 'Guess A Number!'                   =");
+        System.out.println("\n=   I am thinking of a number between 1 and 10.    =\n");
 
-        // Generate the secretNumber once at the beginning of the game
         secretNumber = random.nextInt(10) + 1;
 
-        while (keepRunning == true && numberOfGuesses < 3) {
+        do {
             numberOfGuesses++;
-            System.out.println("[What  is  your  guess?]");
-            guessedNumber = keyboard.nextInt();
+            System.out.print("=   Enter your guess: ");
+
+            try {
+                guessedNumber = keyboard.nextInt();
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("=   Invalid input. Please enter a number.");
+                keyboard.nextLine(); // Clear the invalid input
+                continue;
+            }
+
             if (guessedNumber < secretNumber) {
-                System.out.println("No, mine is higher.");
+                System.out.println("\n=   Your guess is too low. Try a higher number.");
             } else if (guessedNumber > secretNumber) {
-                System.out.println("No, mine is lower.");
+                System.out.println("\n=   Your guess is too high. Try a lower number.");
             } else {
-                System.out.println("Exactly! You're really good!");
+                System.out.println("\n=   [YAY! YOU'VE GOT IT!]");
                 keepRunning = false;
             }
-            System.out.println("You have " + (3 - numberOfGuesses) + " guesses left.");
-        }
-        System.out.println("\nThanks for playing! The correct number was: " + secretNumber);
+
+            System.out.println("=   You have " + (MAX_GUESSES - numberOfGuesses) + " guesses left.\n");
+        } while (keepRunning && numberOfGuesses < MAX_GUESSES);
+
+        System.out.println("\n=   Thanks for playing! The correct number was: [" + secretNumber + "]");
     }
 }
+
